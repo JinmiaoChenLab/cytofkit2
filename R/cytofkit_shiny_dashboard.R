9af08be676f1e0798b4f28c500ef3cdcd3322de9
raman91 <ramansethi91@gmail.com>
2021-05-10 16:09:36 +0800

    Add files via upload

---
+++ R/cytofkit_shiny_dashboard.R
@@ -0,0 +1,22 @@
+#' Title
+#'
+#' @param onSever Whether the app is running on a server.
+#' @param port Manually set a port, default is randomly allocated.
+#'
+#' @return
+#' @export
+#' @examples 
+  #cytofkit_shiny_dashboard()  
+cytofkit_shiny_dashboard = function(onServer = F, port = NULL){
+  if(isTRUE(onServer)){
+    host <- "0.0.0.0"
+  }else{
+    host <- "127.0.0.1"
+  }
+  if(!is.null(port)){
+    shiny::runApp(system.file('shiny', package = 'cytofkit2'), host = host,
+                  port = port)
+  } else {
+    shiny::runApp(system.file('shiny', package = 'cytofkit2'), host = host)
+  }
+}
